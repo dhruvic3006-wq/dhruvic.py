43b8e17ec6a875b7a863af4e45eed77895bfb18f
dhruvic3006-wq <dhruvic3006@gmail.com>
2026-02-24 09:16:41 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,19 @@
+#Print numbers from 1 to 10
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+#Sum of first n natural numbers
+n=int(input("Enter:"))
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+    print("sum=",s)
+#Table of a number
+num=int(input("Enter number:"))
+i=1
+while i<=10:
+    print(num,"x",i,"=",num+i)
+    i=i+1
