3c4c5522c0a95a43a8c45074abcfeed37df94803
dhruvic3006-wq <dhruvic3006@gmail.com>
2026-03-07 08:13:13 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,12 @@
+#positive indexing
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[0])#first element
+print(arr[2])#third element
+print(arr[4])#fifth element
+#negative indexing
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])#last element
+print(arr[-2])#second element
+print(arr[-5])#first element
