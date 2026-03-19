b8fc9ce35c8fd6bbe4cb1c71ff1075b70938c95e
dhruvic3006-wq <dhruvic3006@gmail.com>
2026-03-19 10:54:39 +0530

    Add files via upload

---
+++ keywordargu.py
@@ -0,0 +1,12 @@
+#basic
+def student_info(name,age,city):
+    print("Name:",name)
+    print("Age:",age)
+    print("City:",city)
+student_info(age=18,city="Rajkot",name="Ravi")
+#mixing positional and keyword
+def display(a,b,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+display(a=3,b=2,c=1)
